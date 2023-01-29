2cb282c6a9837dd62f232770fbed328d66320616
Zenon-Nowakowski <70614514+Zenon-Nowakowski@users.noreply.github.com>
2023-01-29 12:19:14 -0600

    fixing sort

--- Project1/sort.cs
+++ Project1/sort.cs
@@ -8,13 +8,13 @@ namespace Program1
             //Create an array of names out of provided array of lines, break down into collumns
             for(int k = 0; k < lines.Count(); k++)
             {
-                //var names = lines[k].Split(new string[]{","}, StringSplitOptions.RemoveEmptyEntries);
-                //narray[k] = names[3];
+                var names = lines[k].Split(new string[]{","}, StringSplitOptions.RemoveEmptyEntries);
+                narray[k] = names[3];
             }
             //Using created name array, compare that and flip the lines array if name is not in correct spot, bubble sort array0 by using array1 essentially
-            for(int j = 1; j < lines.Length - 1; j++)
+            for(int j = 1; j < lines.Length; j++)
             {   
-                for(int i = 1; i < lines.Length - 2; i++)
+                for(int i = 1; i < lines.Length - 1; i++)
                 {
                     //if narray[i] > narray[i+1] swap 
                     if(narray[i].CompareTo(narray[i+1]) > 0) 
