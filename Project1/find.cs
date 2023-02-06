270e609cb5d260f074ff8f4b8d360b48c57c39c2
Zenon-Nowakowski <zenon.nowakowski8@gmail.com>
2023-02-06 16:32:01 -0600

    work

--- Project1/find.cs
+++ Project1/find.cs
@@ -1,55 +1,94 @@
 using System;
 using System.Linq;
-using System.Xml.Linq;
 
 namespace Program1
 {
     class find
     {
-        Worker W = new Worker();
+        Employee employ = new Employee();
+        public override string ToString()
+        {
+            return
+            employ.workType +
+            employ.ID +
+            employ.fName +
+            employ.lName + 
+            employ.employDate;
+        }
         public void namegen(string[] lines)
         {//generates all names from provided txt file 
             Console.WriteLine("Retrieving names...");
-            for(int i = 1; i < lines.Count(); i++)
-            { 
+            for (int i = 1; i < lines.Count(); i++)
+            {
                 //names array splits along ',' lines 
-                var names = lines[i].Split(new string[]{","}, StringSplitOptions.RemoveEmptyEntries);
+                var names = lines[i].Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
                 Console.WriteLine(names[3]);
             }
         }
         public void findPreHire(string[] lines)
-        {   //for loop to go through list, pick out prehires 
-            for(int i = 1; i < lines.Count(); i++)
+        {   //declare list of prehires 
+            List<PreHired> preHiredList = new List<PreHired>();
+            //for loop to go through list, pick out prehires 
+            for (int i = 1; i < lines.Count(); i++)
             {
-                var names = lines[i].Split(new string[]{","}, StringSplitOptions.RemoveEmptyEntries);
-                if(names[0] == "PreHire")
+                var names = lines[i].Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                if (names[0] == "PreHire")
                 {
-                    Console.WriteLine(names[3]);
+                    Console.WriteLine("Added " + names[3]);
+                    preHiredList.Add(new PreHired
+                    {
+                        workType = names[0],
+                        ID = names[1],
+                        fName = names[2],
+                        lName = names[3],
+                        employDate = names[4],
+                        extended = "",
+                        accepted = ""
+                    });
                 }
             }
         }
         public void findEmployees(string[] lines)
-        {   //create list element of type employee 
-            var employee = new List<Employee>();
-            //export line 0, the headers
+        {   //create list of objects: employees
+            var employeeList = new List<Employee>();
             //for loop to go through list, pick out prehires 
-            for(int i = 1; i < lines.Count(); i++)
+            for (int i = 1; i < lines.Count(); i++)
             {
-                var names = lines[i].Split(new string[]{","}, StringSplitOptions.RemoveEmptyEntries);
-                if(names[0] == "Employee")
+                var names = lines[i].Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                if (names[0] == "Employee")
                 {
-                    Console.WriteLine(names[3]);
+                    Console.WriteLine("Added " + names[3]);
+                    employeeList.Add(new Employee
+                    {
+                        workType = names[0],
+                        ID = names[1],
+                        fName = names[2],
+                        lName = names[3],
+                        employDate = names[4],
+                        title = names[5],
+                        salary = names[8]
+                    });
                 }
             }
         }
         public void findRetirees(string[] lines)
-        {   //for loop to go through list, pick out prehires 
-            for(int i = 1; i < lines.Count(); i++)
+        {   //create list of retirees as objects 
+            List<Retiree> retireeList = new List<Retiree>();
+            //for loop to go through list, pick out prehires 
+            for (int i = 1; i < lines.Count(); i++)
             {
-                var names = lines[i].Split(new string[]{","}, StringSplitOptions.RemoveEmptyEntries);
-                if(names[0] == "Retiree")
+                var names = lines[i].Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                if (names[0] == "Retiree")
                 {
-                    Console.WriteLine(names[3]);
+                    Console.WriteLine("Added " + names[3]);
+                    retireeList.Add(new Retiree
+                    {
+                        workType = names[0],
+                        ID = names[1],
+                        fName = names[2],
+                        lName = names[3],
+                        employDate = names[4],
+                    });
                 }
             }
         }
--- Project1/types.cs
+++ Project1/types.cs
@@ -5,10 +5,10 @@ namespace Program1
     public class Worker
     {
         public string workType {get; set;} = default!;
-        public int ID {get; set;} = default!;
+        public string ID {get; set;} = default!;
         public string fName {get; set;} = default!;
         public string lName {get; set;} = default!;
-        public string lemployDate {get; set;} = default!;
+        public string employDate {get; set;} = default!;
     }
     public class PreHired : Worker
     {
@@ -18,7 +18,7 @@ namespace Program1
     public class Employee : Worker
     {
         public string title {get; set;} = default!;
-        public double salary {get; set;} = default!;
+        public string salary {get; set;} = default!;
     }
     public class Retiree : Worker
     {
